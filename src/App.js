ad56daa29b62d27161aa6eedc944611d5e6faad4
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-15 16:22:10 +0100

    src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TradingScanner from './components/TradingScanner';
+
+function App() {
+  return (
+    <div className="App">
+      <TradingScanner />
+    </div>
+  );
+}
+
+export default App;
